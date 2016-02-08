0d6c2d454d214133ce48ce43d2c33b07493b39ad
Emmanuel Vodor <emmanuelvodor@stratis.fr>
2016-02-08 10:13:52 +0100

    remove cmd.php

--- cmd.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Usage: cmd.php config.yaml...
-require __DIR__ . '/vendor/autoload.php';
-
-use Stratis\Component\Migrator\Migrator;
-
-$migrator = new Migrator($argv[1]);
-$migrator->process();
