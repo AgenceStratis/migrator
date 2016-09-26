4dfe2409277508a3a22f9f8321ba66188a7ee623
Emmanuel Vodor <emmanuelvodor@stratis.fr>
2016-09-26 17:23:55 +0200

    [FEATURE] Added sync task to transfer files

---
+++ src/Task/SyncTask.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Stratis\Component\Migrator\Task;
+
+use Stratis\Component\Migrator\Configuration;
+use Hoa\File;
+
+/**
+ * Class SyncTask
+ * @package Stratis\Component\Migrator\Task
+ * @config array $sources
+ */
+class SyncTask extends DefaultTask
+{
+    /**
+     * @param Configuration $config
+     * @throws \Exception
+     */
+    public function main(Configuration $config)
+    {
+        $remote = $config->get(array('remote'), '');
+        $local = $config->get(array('local'), '');
+
+        if (empty($remote)) {
+            throw new \Exception('Task/Sync: No remote path defined.');
+        }
+
+        if (empty($local)) {
+            throw new \Exception('Task/Sync: No local path defined.');
+        }
+
+        if (count($this->sources) > 1) {
+            throw new \Exception('Task/Sync: This task uses only 1 source.');
+        }
+
+        // Trim slashes from paths
+        $remote = rtrim($remote, '/');
+        $local = rtrim($local, '/');
+
+        // Use only one source
+        reset($this->sources);
+        $source = current($this->sources);
+
+        // Download files from remote server
+        foreach ($source as $record) {
+            $file = new File\Read($remote . '/' . $record['file']);
+            $file->copy($local . '/' . $record['file']);
+        }
+
+        exit;
+    }
+}
